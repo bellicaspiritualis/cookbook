7f345d865be510cf517b0c5722f19689f26035dd
root zona <rootzona@protonmail.com>
2022-12-21 23:48:14 +0100

    fun getRecipes i getIngredints w AddFragment

--- app/src/main/java/bellica/spiritualis/cookbook/add/AddFragment.kt
+++ app/src/main/java/bellica/spiritualis/cookbook/add/AddFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import bellica.spiritualis.cookbook.databinding.FragmentAddBinding
 import bellica.spiritualis.cookbook.databinding.ItemIngredientBinding
+import bellica.spiritualis.cookbook.models.Ingredient
+import bellica.spiritualis.cookbook.models.Recipe
 
 class AddFragment: Fragment() {
 
@@ -38,6 +41,7 @@ class AddFragment: Fragment() {
 
     private fun initClickListeners() {
         binding.saveButton.setOnClickListener {
+            val recipe = getRecipes()
             findNavController().popBackStack()
         }
 
@@ -50,5 +54,31 @@ class AddFragment: Fragment() {
         val container = binding.ingredientsContainer
         val ingredientItem = ItemIngredientBinding.inflate(layoutInflater, container, false)
         container.addView(ingredientItem.root)
+    }
+
+    private fun getRecipes() = Recipe(
+        id = 0L,
+        title = binding.title.text.toString(),
+        ingredients = getIngredients(),
+        description = binding.description.text.toString()
+    )
+
+    private fun getIngredients(): List<Ingredient> {
+        val ingredientsChildren = binding.ingredientsContainer.children
+        val ingredients = mutableListOf<Ingredient>()
+
+        for(ingredientView in ingredientsChildren) {
+            val ingredientBinding = ItemIngredientBinding.bind(ingredientView)
+            val ingredient = with(ingredientBinding) {
+                Ingredient(
+                    title = name.text.toString(),
+                    amount = amount.text.toString().replace(",", ".").toDouble(),
+                    measureType = unit.text.toString()
+                )
+            }
+            ingredients.add(ingredient)
+        }
+
+        return ingredients
     }
 }
